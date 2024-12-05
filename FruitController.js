28547945646395028115a0ea378e298273a4d73c
Marsela0603 <marselasela3554@gmail.com>
2024-12-05 14:01:23 +0700

    tugas 9

--- FruitController.js
+++ FruitController.js
@@ -14,5 +14,21 @@ const store = (name) => {
   index();
 };
 
+// membuat fungsi update 
+const update = (indexToUpdate, newName) => {
+  if (indexToUpdate >= 0 && indexToUpdate < fruits.length) {
+    fruits[indexToUpdate] = newName;
+    index();
+  }
+};
+
+// membuat fungsi destroy 
+const destroy = (indexToDelete) => {
+  if (indexToDelete >= 0 && indexToDelete < fruits.length) {
+    fruits.splice(indexToDelete, 1);
+    index();
+  }
+};
+
 // export method index dan store
-module.exports = { index, store };
+module.exports = { index, store, update, destroy };
--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // import FruitController
-const { index, store } = require("./FruitController.js");
+const { index, store, update, destroy } = require("./FruitController.js");
 
 // membuat fungsi main
 const main = () => {
+  // menampilkan semua data
   index();
+
+  // menambah data pisang
   store("Pisang");
+
+  // mengubah data ke 0 menjadi "kelapa"
+  update(0, "Kelapa");
+
+  // menghapus data ke 0
+  destroy(0);
 };
 
 main();
